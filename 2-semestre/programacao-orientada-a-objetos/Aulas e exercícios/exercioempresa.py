2b44495432e38e8e571647b099edfb51d1fca30b
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-03 11:18:59 -0300

    forcei os atributos a ser int e str

--- 2-semestre/programacao-orientada-a-objetos/Aulas e exercícios/exercioempresa.py
+++ 2-semestre/programacao-orientada-a-objetos/Aulas e exercícios/exercioempresa.py
@@ -1,8 +1,9 @@
 class Departamentos:
-    def __init__(self, cod_departamento, nome_departamento, nome_gerente):
+    def __init__(self, cod_departamento : int, nome_departamento : str, nome_gerente : str):
         self.cod_departamento = int(cod_departamento)
         self.nome_depto = nome_departamento
         self.nome_gerente = nome_gerente
+        self.gerente_anterior = None
 
     def listar_departamento(self):
         dados_depto = f"""\n==========DADOS==========
@@ -12,14 +13,12 @@ Nome do gerente = {self.nome_gerente}"""
         print(dados_depto)
 
     def mudar_gerente(self, novo_gerente):
-        print("\nNome do gerente atualizado!")
-        print(f"Gerente anterior: {self.nome_gerente}")
+        self.gerente_anterior = self.nome_gerente
         self.nome_gerente = novo_gerente
-        print(f"Gerente atual: {self.nome_gerente}")
         dados_depto = f"""\n==========DADOS==========
 Código do departamento = {self.cod_departamento}
 Nome do departamento = {self.nome_depto}
-Nome do gerente = {self.nome_gerente}"""
+Nome do gerente = Anterior: {self.gerente_anterior} | Atual: {self.nome_gerente}"""
         print(dados_depto)
 
 departamento1 = Departamentos('101', 'Marketing', 'Paulo')
@@ -30,3 +29,4 @@ departamento1.listar_departamento()
 departamento2.listar_departamento()
 departamento3.listar_departamento()
 departamento1.mudar_gerente('Marcos')
+departamento2.mudar_gerente(input('\nNome do novo gerente? '))
