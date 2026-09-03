e3c3777bdf91250f46acfd00f8b6eb70db0bf8c2
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-03 14:53:26 -0300

    Receita de filé de peixe usando Classes

--- 2-semestre/programacao-orientada-a-objetos/Atividades/receita1.py
+++ 2-semestre/programacao-orientada-a-objetos/Atividades/receita1.py
@@ -1,21 +1,38 @@
 class Receita:
-    def __init__(self, nome_chef, nome_receita :str, rendimento : int, tempo_preparo : int, ingredientes : dict):
-        self.nome_chef = None
+    def __init__(self, nome_receita :str, rendimento : float, tempo_preparo : float, ingredientes : dict):
         self.nome_receita = nome_receita
         self.rendimento = rendimento
         self.tempo_preparo = tempo_preparo
         self.ingredientes = ingredientes
 
-    def perguntar_nome(self):
-        self.nome_chef = input("Digite seu nome: ")
-        return self.nome_chef
 
     def calcular_porcoes(self, porcoes_desejadas : int):
         fator = porcoes_desejadas / self.rendimento
         return fator
 
-receita_peixe = Receita(None, 'Filé de peixe no papelote com legumes', 1, 15,
+    def exibir_ficha_tecnica(self, nome_chefe:str, porcoes_desejadas:int):
+        fator = self.calcular_porcoes(porcoes_desejadas)
+        print(f"----------FICHA TÉCNICA----------")
+        print(f"RECEITA: {self.nome_receita}")
+        print(f"Chefe responsável: {nome_chefe}")
+        print(f"---------------------------------")
+        print(f"Rendimento: {porcoes_desejadas} porção(ões)")
+        print(f"Tempo total {self.tempo_preparo} minutos")
+        print(f"---------------------------------")
+        print(f"INGREDIENTES NECESSÁRIOS:")
+        for item, qntd_base in self.ingredientes.items():
+            qntd_ajustada = qntd_base * fator
+            print(f"{qntd_ajustada:.1f} {item}")
+        print("2 colheres de sopa de azeite")
+        print("Sal a gosto")
+receita_peixe = Receita('Filé de peixe no papelote com legumes', 1, 15,
                         {'filé de peixe': 1,
                          'batata': 2,
                          'cenoura': 1,
                          'abobrinha': 1})
+
+if __name__ == "__main__":
+    nome_usuario = input("Digite seu nome: ")
+    porcoes_usuario =  int(input("Quantas porções você deseja preparar? "))
+
+    receita_peixe.exibir_ficha_tecnica(nome_usuario, porcoes_usuario)
