7458d6e090763cde504b302025df64fe627ad0a5
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-04 09:52:37 -0300

    Aula 4 e php

---
+++ 2-semestre/programacao-web/Aulas/Aula 04/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante João Protagonista</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header id="topo">
+        <div class="logo">LOGO</div>
+        <nav>
+            <ul class="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="cardapio.php">Cardápio</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+                <li><a href="reservas.php">Reservas</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="banner">
+        <div class="infos">
+            <h2>Conheça nossas</h2>
+            <h1>Receitas Especiais</h1>
+            <p>Venha conhecer nossos pratos com receitas exclusivas vindas direto da Ilha das Cobras e dos Gatos localizados na Malásia</p>
+        </div>
+        <div class="imagem"></div>
+    </section>
+    <section class="sobre">
+        <div class="infos">
+            <h2>Conheça nossa história</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae autem aperiam debitis cumque, esse mollitia dolores incidunt vitae eos dolore nostrum inventore odit, ipsa excepturi, consequuntur qui cum! Quo, ducimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam delectus natus repellat aliquid dolorem distinctio, pariatur excepturi fugit unde! Veniam optio error ratione placeat labore quasi quidem voluptates deleniti repellat.</p>
+        </div>
+        <div class="imagem"></div>
+    </section>
+    <section class="pratos">
+        <div class="container">
+            <div class="row">
+                <div class="imagem"></div>
+                <h3>Receita 1</h3>
+            </div>
+            <div class="container">
+            <div class="row">
+                <div class="imagem"></div>
+                <h3>Receita 2</h3>
+            </div>
+            <div class="container">
+            <div class="row">
+                <div class="imagem"></div>
+                <h3>Receita 3</h3>
+            </div>
+            <div class="container">
+            <div class="row">
+                <div class="imagem"></div>
+                <h3>Receita 4</h3>
+            </div>
+        </div>
+    </section>
+    <footer class="rodape">
+        <p>Todos os direitos reservados &copy; 2026 Restaurante João Protagonista</p>
+    </footer>
