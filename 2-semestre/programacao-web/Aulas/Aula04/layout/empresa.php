384b2a248eed99c646afa681a5f71ba5de10ebb5
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-04 11:22:33 -0300

    Novas files .php

---
+++ 2-semestre/programacao-web/Aulas/Aula04/layout/empresa.php
@@ -0,0 +1,5 @@
+<div class="infos">
+            <h2>Conheça nossa história</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae autem aperiam debitis cumque, esse mollitia dolores incidunt vitae eos dolore nostrum inventore odit, ipsa excepturi, consequuntur qui cum! Quo, ducimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam delectus natus repellat aliquid dolorem distinctio, pariatur excepturi fugit unde! Veniam optio error ratione placeat labore quasi quidem voluptates deleniti repellat.</p>
+        </div>
+        <div class="imagem"><img src="images/empresa.jpg" alt="Prato"></div>
---
+++ 2-semestre/programacao-web/Aulas/Aula04/layout/topo.php
@@ -0,0 +1,11 @@
+<header id="topo">
+        <div class="logo"><img src="images/garfologo.png" alt="Garfo e faca Logo"></div>
+        <nav>
+            <ul class="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="cardapio.php">Cardápio</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+                <li><a href="reservas.php">Reservas</a></li>
+            </ul>
+        </nav>
+    </header>
