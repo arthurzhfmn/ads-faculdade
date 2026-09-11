8150ca4e0899e26f5d2aabaa24224d4b9d64d5e8
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-11 11:32:09 -0300

    Código da aula 06 completo, validando form com php

--- 2-semestre/programacao-web/Aulas/Aula06/index.php
+++ 2-semestre/programacao-web/Aulas/Aula06/index.php
@@ -11,10 +11,46 @@
 <body>
     <!-- method get/post (n mostra os dados na URL) -->
     <form action="" method="post">
-        <input type="text" name="nome" placeholder="Digite seu nome">
+        <input type="text" name="nome" placeholder="Digite seu nome" required>
         <input type="email" name="email" placeholder="seuemail@email.com">
-        <input type="number" name="idade" placeholder="Digite sua idade">
+        <input type="number" name="idade" placeholder="Digite sua idade" value="18">
         <input type="submit" name="enviar">
     </form>
 </body>
 </html>
+
+<?php
+    // No php, os dados enviados via post são recuperados pela var $_POST
+    if (isset($_POST['enviar'])) {
+        //Validando campos no backend
+        $erros = [];
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+
+        if (strlen(trim($nome)) <= 3) { //trim() retira os espaços em branco de uma string
+            $erros[] = "Preencha seu nome completo";
+        }
+
+        if(empty($email)){
+            $erros[] = "Preencha seu email corretamente";
+        }
+
+        if($idade >=18 && $idade <=100){
+            $erros[] = "Cadastro somente para maiores de 18 anos";
+        }
+
+        if(empty($erros)) {
+            // Continuar o envio do formulário
+            echo "<h2>Confira seus dados: </h2>";
+            echo "Nome: $nome  <br>";
+            echo "Email: $email  <br>";
+            echo "Idade: $idade  ";           
+        }else{
+            foreach ($erros as $erro){
+                echo "$erro <br>";
+            }
+        }
+    }
+
+?>
