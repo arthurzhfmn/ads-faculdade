39205cd2f65410b15a4e972214005f2631b56fcd
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-18 09:47:49 -0300

    Codigo pronto

---
+++ 2-semestre/programacao-web/Atividades/phpgrupos/funcoes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ucwords $ ucfirst</title>
+    <style>input{display:block; margin-bottom: 8px; padding: 8px 5px;}</style>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="nome" placeholder="Digite seu primeiro nome">
+        <input type="text" name="sobrenome" placeholder="Digite seu sobrenome completo">
+        <input type="text" name="filme_fav" placeholder="Filme favorito">
+        <input type="submit" name="enviar">
+    </form>
+</body>
+</html>
+<?php 
+
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$filme_fav = filter_input(INPUT_POST, 'filme_fav', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+if(isset($_POST['enviar'])){
+    echo "<h2>Confira seus dados: </h2>";
+    echo "<p>Nome: ".ucfirst($nome) ."</p>";
+    echo "<p>Sobrenome: " .ucwords($sobrenome) ."</p>";
+    echo "Filme favorito: " .ucwords($filme_fav);
+}
+?>
