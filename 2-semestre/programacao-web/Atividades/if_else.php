56db2ff166ecba4027e49cfdca3b32e8a5194208
Arthur Hoffmann <arthurzfhoffmann@gmail.com>
2026-09-25 22:36:06 -0300

    update file

--- 2-semestre/programacao-web/Atividades/if_else.php
+++ 2-semestre/programacao-web/Atividades/if_else.php
@@ -7,7 +7,7 @@ if ($latencia == 0) {
 } elseif ($latencia <= 50) {
     echo "OK: Conexão Excelente";
 } elseif ($latencia <= 150) {
-    echo "WARNING: Conexão Aceitável, mas requer atenção";
+    echo "Aviso: Conexão Aceitável, mas requer atenção";
 } else {
     echo "ERRO: Latência Alta! Verifique o link.";
 }
